7f844d079445f438a6cf595463d3877ae28579b4
rajj0208 <raj@LAPTOP-JKCA4JH3.localdomain>
2020-05-07 18:47:04 +0530

    testing

--- app/src/models/compute.py
+++ app/src/models/compute.py
@@ -25,7 +25,7 @@ class Compute:
     def factorial(n):
         fact=1
         if n==0:
-            return 0
+            return 1
         else:
             for i in range(1,n+1):
                 fact=fact*i
