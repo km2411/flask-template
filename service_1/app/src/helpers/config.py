25daa6ee4042138b5da8cddd486e63629c83902b
Kartik Mittal <kkartikmittal@gmail.com>
2020-05-17 09:12:12 +0530

    fixed compose file

--- service_1/app/src/helpers/config.py
+++ service_1/app/src/helpers/config.py
@@ -1,1 +1,1 @@
-PRECOMPUTE_SERVICE = 'http://10.10.10.3:9090/precomputed' # hardcoded here but ideally fetched from env
+PRECOMPUTE_SERVICE = 'http://service-2:9090/precomputed' # hardcoded here but ideally fetched from env
