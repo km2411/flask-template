6733c2f025887baf30a8848275502be46b170494
Kartik Mittal <kkartikmittal@gmail.com>
2020-05-17 18:57:40 +0530

    fix merge

--- service_1/app/src/helpers/config.py
+++ service_1/app/src/helpers/config.py
@@ -2,4 +2,4 @@ import os
 SERVICE2 = os.environ.get('SERVICE2')
 SERVICE2_PORT = os.environ.get('SERVICE2_PORT')
 
-PRECOMPUTE_SERVICE = 'http://' + SERVICE2 + ':' + SERVICE2_PORT +'/precomputed' 
+PRECOMPUTE_SERVICE = 'http://' + SERVICE2 + ':' + SERVICE2_PORT +'/precomputed'
