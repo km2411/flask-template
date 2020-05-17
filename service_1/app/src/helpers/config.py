f80dc0b3c2cf32f2f7e743aa19b14df0b4faaea9
Kartik Mittal <kkartikmittal@gmail.com>
2020-05-17 10:08:05 +0530

    use env vars

--- service_1/app/src/helpers/config.py
+++ service_1/app/src/helpers/config.py
@@ -1,1 +1,5 @@
-PRECOMPUTE_SERVICE = 'http://service-2:9090/precomputed' # hardcoded here but ideally fetched from env
+import os
+SERVICE2 = os.environ.get('SERVICE2')
+SERVICE2_PORT = os.environ.get('SERVICE2_PORT')
+
+PRECOMPUTE_SERVICE = 'http://' + SERVICE2 + ':' + SERVICE2_PORT +'/precomputed' 
